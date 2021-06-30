555db18bf7a9266ae8c02664d27d97804e5d32ad
nikolay <nikolas-ja@meta.ua>
2021-06-30 11:31:53 +0300

    create db

--- application/core/model.php
+++ application/core/model.php
@@ -7,49 +7,33 @@ class Model
     {
         // создание базы данных
         try {
-        	$link = new PDO("mysql:host=localhost", "root", "111");
+        	$link = new PDO("mysql:host=localhost", "nikolay", "nikolay");
         }
        	catch (PDOException $e) {
-       		exit('Отсутствует соединение с базой данных');
+       		exit('not comnnect db');
        	} 
-        $link->query('CREATE DATABASE IF NOT EXISTS guestbook');
+        $link->query('CREATE DATABASE IF NOT EXISTS adressbook');
         // создание таблиц
 		try {        
-        $link = new PDO("mysql:host=localhost; dbname=guestbook", "root", "111");
+        $link = new PDO("mysql:host=localhost; dbname=adressbook", "nikolay", "nikolay");
         }    
        	catch (PDOException $e) {
-       		exit('Отсутствует соединение с базой данных');
+       		exit('not comnnect db');
        	} 
-        $link->query('CREATE TABLE IF NOT EXISTS guestbook(id_guestbook INTEGER PRIMARY KEY AUTO_INCREMENT, date DATETIME, name CHAR(20), email CHAR(50), comment TEXT)');
-        $link->query('CREATE TABLE IF NOT EXISTS users(id_user INTEGER PRIMARY KEY AUTO_INCREMENT, login CHAR(30), password CHAR(255))');
+		$link->query('CREATE TABLE IF NOT EXISTS adress(id INTEGER PRIMARY KEY AUTO_INCREMENT, date DATETIME, name CHAR(50), surname CHAR(50), phone CHAR(50), street CHAR(50), city CHAR(50), country CHAR(50))');
         $this->link = $link;
     }
-
-	function linkGuest()
-	{
-		$this->link->query("CREATE USER IF NOT EXISTS guest@localhost IDENTIFIED BY '123'");
-		$this->link->query("GRANT SELECT,INSERT ON guestbook.guestbook TO guest@localhost");
-		$this->link->query("FLUSH PRIVILEGES");
-		try {
-		$linkGuest = new PDO("mysql:host=localhost; dbname=guestbook", "guest", "123");
-		}
-       	catch (PDOException $e) {
-       		exit('Отсутствует соединение с базой данных');
-       	} 
-		return $linkGuest;
-	}
 
     function linkAdmin()
     {
     	$this->link->query("CREATE USER IF NOT EXISTS admin@localhost IDENTIFIED BY '12345'");
-		$this->link->query("GRANT SELECT,DELETE,INSERT,UPDATE ON guestbook.guestbook TO admin@localhost");
-		$this->link->query("GRANT SELECT,UPDATE ON guestbook.users TO admin@localhost");
+		$this->link->query("GRANT SELECT,DELETE,INSERT,UPDATE ON adressbook.adressbook TO admin@localhost");
 		$this->link->query("FLUSH PRIVILEGES");
 		try {
-		$linkAdmin = new PDO("mysql:host=localhost; dbname=guestbook", "admin", "12345");
+		$linkAdmin = new PDO("mysql:host=localhost; dbname=adressbook", "admin", "12345");
 		}
        	catch (PDOException $e) {
-       		exit('Отсутствует соединение с базой данных');
+       		exit('not comnnect db');
        	} 
 		return $linkAdmin;    	
     }
