baacf5f94a86941e5ab8b10093dc48d5e954401d
nikolay <nikolas-ja@meta.ua>
2021-07-09 11:59:40 +0300

    fix input

--- application/views/adress/form-adress.php
+++ application/views/adress/form-adress.php
@@ -1,7 +1,7 @@
-   <p>name:&nbsp;<input name="name" type="text" value="<?php echo $res['name'] ?? ''; ?>" required></p>
-   <p>surname:&nbsp;<input name="surname" type="text" value="<?php echo $res['surname'] ?? ''; ?>" required></p>
-   <p>phone:&nbsp;<input name="phone" type="text" value="<?php echo $res['phone'] ?? ''; ?>" required></p>
-   <p>street:&nbsp;<input name="street" type="text" value="<?php echo $res['street'] ?? ''; ?>" required></p>
-   <p>city:&nbsp;<input name="city" type="text" value="<?php echo $res['city'] ?? ''; ?>" required></p>
-   <p>country:&nbsp;<input name="country" type="text" value="<?php echo $res['country'] ?? ''; ?>" required></p>
+   <p>name:&nbsp;<input name="name" type="text" value="<?php echo isset($res) ? htmlentities($res['name'], ENT_QUOTES) : ''; ?>" required></p>
+   <p>surname:&nbsp;<input name="surname" type="text" value="<?php echo isset($res) ? htmlentities($res['surname'], ENT_QUOTES) : ''; ?>" required></p>
+   <p>phone:&nbsp;<input name="phone" type="text" value="<?php echo isset($res) ? htmlentities($res['phone'], ENT_QUOTES) : ''; ?>" required></p>
+   <p>street:&nbsp;<input name="street" type="text" value="<?php echo isset($res) ? htmlentities($res['street'], ENT_QUOTES) : ''; ?>" required></p>
+   <p>city:&nbsp;<input name="city" type="text" value="<?php echo isset($res) ? htmlentities($res['city'], ENT_QUOTES) : ''; ?>" required></p>
+   <p>country:&nbsp;<input name="country" type="text" value="<?php echo isset($res) ? htmlentities($res['country'], ENT_QUOTES) : ''; ?>" required></p>
    <p><input type="submit"></p>
